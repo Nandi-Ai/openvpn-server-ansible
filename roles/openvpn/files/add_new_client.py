fc0676a1fb57b2843124ad0295e433e0842b7873
Hubert Mach <hubert.mach@gmail.com>
2020-06-30 09:58:48 +0200

    Adjust script to use given config file name

--- roles/openvpn/files/add_new_client.py
+++ roles/openvpn/files/add_new_client.py
@@ -1,15 +1,8 @@
-import re
-import subprocess
 import os
-import pprint
-import shlex
-import subprocess
 from ipaddress import IPv4Network
 from os import listdir
 from os.path import isfile, join
 import argparse
-import random
-import string
 
 # Globals
 CLIENT_CONFIGS_PATH = "/root/client-configs"
@@ -18,26 +11,12 @@ MAKE_NEW_KEYS_SCRIPT_PATH = join(CLIENT_CONFIGS_PATH, "add_new_keys.sh")
 CCD = "/etc/openvpn/ccd"
 NETWORK = IPv4Network('10.8.0.0/24')
 RESERVED_IP = []
-CLIENT_PREFIX = 'client'
-MAX_CLIENT_ID = 0
-
-def randomString(stringLength=8):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
 
 def get_new_client_data():
 
-    global MAX_CLIENT_ID
     ccd_files = [f for f in listdir(CCD) if isfile(join(CCD, f))]
 
     for ccd_file in ccd_files:
-
-        # get number of client
-        res = re.search("^"+CLIENT_PREFIX+"(\\d+)", ccd_file)
-        if int(res.group(1)) > MAX_CLIENT_ID:
-            MAX_CLIENT_ID = int(res.group(1))
-
         with open(join(CCD, ccd_file), 'r') as ccdf:
             for line in ccdf.readlines():
                 if 'ifconfig-push' in line:
@@ -46,22 +25,17 @@ def get_new_client_data():
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('-n', '--name', default='empty', help='Selected Name')
+    argparser.add_argument('-n', '--name', required=True, help='Selected Name')
     args = argparser.parse_args()
-    NAME=args.name
-    if NAME == 'empty':
-        NAME=randomString()
-#        get_new_client_data()
-#        MAX_CLIENT_ID += 1
-#        new_ccd_file = "%s%d" % (CLIENT_PREFIX, MAX_CLIENT_ID)
-    new_ccd_file=NAME
+
+    new_ccd_file = args.name
     hosts_iterator = (host for host in NETWORK.hosts() if str(host) not in RESERVED_IP)
     new_ip = next(hosts_iterator)
     entry = "ifconfig-push %s 255.255.255.255" % (new_ip)
 
     # Generate new certs
     print("Generating new certs for %s" % new_ccd_file)
-    
+
     os.system(MAKE_NEW_KEYS_SCRIPT_PATH+" "+new_ccd_file)
 
     # Execute script to generate new openvpn file
