0bb69c0b6e8e1124707ef2d08a299839d8f5367c
Hubert Mach <hubert.mach@gmail.com>
2020-09-11 09:58:00 +0200

    Update bootstrap.sh cron job creation

--- roles/nginx/files/bootstrap.sh
+++ roles/nginx/files/bootstrap.sh
@@ -3,7 +3,7 @@
 # Script for installing required software and configuration
 
 apt-get update
-# apt-get upgrade -y
+apt-get upgrade -y
 apt-get install -y openvpn
 
 # create scripts directory
@@ -14,14 +14,29 @@ host="remote.nandi.io"
 user="access"
 pass="Dahm3aisee7O"
 
-for FILE in "check_openvpn.sh" "GetOpenVpnClientFile.py" "crontab.tpl"
-do
-	echo "Downloading $FILE..."
-	curl -u access:Dahm3aisee7O  -o /opt/scripts/$FILE http://remote.nandi.io/scripts/$FILE
-done
+FILE="check_openvpn.sh"
+curl -u access:Dahm3aisee7O  -o /opt/scripts/$FILE http://remote.nandi.io/scripts/$FILE
 
-# Copy custom crontab
-cp -rf /opt/scripts/crontab.tpl /etc/cron.d/custom
-systemctl enable ssh
-systemctl start ssh
-systemctl enable openvpn@server.service --now
+FILE="GetOpenVpnClientFile.py"
+curl -u access:Dahm3aisee7O  -o /opt/scripts/$FILE http://remote.nandi.io/scripts/$FILE
+
+# Write out current crontab
+crontab -l > mycron
+
+# Adding new crons
+echo "entry 1"
+cat mycron | grep 'openvpn@server.service'  && echo 'Entry 1 exists' || echo "@reboot systemctl start openvpn@server.service" >> mycron
+
+echo "entry 2"
+cat mycron | grep 'check_openvpn.sh'  && echo 'Entry 2 exists' || echo '00 * * * *	root sh /opt/scripts/check_openvpn.sh' >> mycron
+
+echo "entry 3"
+ENTRY="20 */3 * * *	root python3 /opt/scripts/GetOpenVpnClientFile.py"
+cat mycron | grep 'GetOpenVpnClientFile.py'  && echo 'Entry 3 exists' || echo "20 */3 * * *	root python3 /opt/scripts/GetOpenVpnClientFile.py" >> mycron
+
+# Install new crontab
+crontab mycron
+rm mycron
+echo "Rebooting system"
+reboot
+
